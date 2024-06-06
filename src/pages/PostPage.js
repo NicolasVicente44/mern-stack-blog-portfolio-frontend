3c0a11916eed361dc053aabcabe4860addcfe6e1
Your Name <your_email@example.com>
2024-06-06 15:01:44 -0400

    Updated PostPage component to handle undefined postInfo.author

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -4,10 +4,11 @@ import { formatISO9075 } from "date-fns";
 import { UserContext } from "../UserContext";
 import { Link } from "react-router-dom";
 
-export default function PostPage() {
+export default function PostPage({ post }) {
   const [postInfo, setPostInfo] = useState(null);
   const { userInfo } = useContext(UserContext);
   const { id } = useParams();
+
   useEffect(() => {
     fetch(`https://mern-blog-portfolio-backend.onrender.com/post/${id}`).then(
       (response) => {
@@ -24,8 +25,8 @@ export default function PostPage() {
     <div className="post-page">
       <h1>{postInfo.title}</h1>
       <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
-      <div className="author">by @{postInfo.author.username}</div>
-      {userInfo.id === postInfo.author._id && (
+      <div className="author">by @{postInfo.author?.username}</div>
+      {userInfo && postInfo.author && userInfo.id === postInfo.author._id && (
         <div className="edit-row">
           <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
             <svg
