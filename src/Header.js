02296d1faffcdc239bd75ca2e1564caf9e417d58
Your Name <your_email@example.com>
2024-06-06 15:08:45 -0400

    Updated PostPage component to handle undefined postInfo.author

--- src/Header.js
+++ src/Header.js
@@ -38,6 +38,7 @@ export default function Header() {
         )}
         {!username && (
           <>
+            <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
           </>
         )}
